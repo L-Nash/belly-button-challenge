32a7c79fce1d3c15e3b06e499dbac09658cffd8f
L-Nash <113747132+L-Nash@users.noreply.github.com>
2023-02-11 13:10:46 -0600

    updated demodata

--- static/js/app.js
+++ static/js/app.js
@@ -1,102 +1,107 @@
 //set URL variable
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Establish the Promise
-const dataPromise = d3.json(url);
-
-// Get JSON data and log to console
-let data = dataPromise.then(function(data) {
-  console.log(data.names);
-  console.log(data.samples);
-});
-//----------------------------- link IDS to dropdonw
-
-  // // Use D3 to select the dropdown menu
-  let dropdownMenu = d3.select("#selDataset");
-
-  d3.json(url).then((data)=>{
-    console.log(data.metadata)
-  let PatientIDs = data.names
-  // Loop though options and append to to dropdown
-  PatientIDs.forEach((ID)=>{
-    dropdownMenu.append("option").text(ID)
-  })
-  })
- 
 
 
 //---------------------------filter so starting ID = the data row
-d3.json(url).then((data)=>{
+function makeMetaData(selectedID){
+  d3.json(url).then((data)=>{
 
-// function selectDemo(each) {
-//   return each.id == selectedID;
-// }
+    // --------------------------- use metadata to populate demo data
+    let demoData = data.metadata;
+    for (let i = 0; i < demoData.length; i++) {
+      let demo = demoData[i]
+      if ( demo.id == selectedID ) {
 
-// --------------------------- use metadata to populate demo data
-let demoData = data.metadata;
+        let panel = d3.select(".panel-body");
+        panel.html("");
+       
+
+        for (const [key, value] of Object.entries(demo)) {
+          let text = `${key}: ${value}`;
+          panel.append("h6").text(text);
+        }
+      }
+    }
+  })
+}
+
+
+
+
+// Establish the Promise
+// const dataPromise = d3.json(url);
  //----------------------Set up starting ID
-let selectedID = 940;
+// let selectedID = 940;
+function init(){
+// Get JSON data and log to console
+// let data = dataPromise.then(function(data) {
+//   console.log(data.names);
+//   console.log(data.samples);
+// });
+//----------------------------- link IDS to dropdonw
 
-for (let i = 0; i < 100; i++) {
-  let demo = demoData[i]
-  if ( demo.id == selectedID ) {
+  // // Use D3 to select the dropdown menu
+    let dropdownMenu = d3.select("#selDataset");
+  d3.json(url).then((data)=>{
+    let PatientIDs = data.names;
+    // Loop though options and append to to dropdown
+    PatientIDs.forEach((ID)=>{
+      dropdownMenu.append("option").attr("value", ID).text(ID);
+    })
+    let initialID = PatientIDs[0];
+    // makePlots(initialID);
+    makeMetaData(initialID);
 
-
-  age = Object.entries(demoData)
-  console.log(age);
-  
-
-  
-    
-  // Calling the function
-  // let demoInfo = demoData.filter(selectDemo);
-
-  // test = JSON.stringify(demo); 
-  
-  // Need to extract key value pairs - this won't work...
-  d3.select(".panel-body").append("p").text(demoData[i])
-  // d3.select(".panel-body").append("li").text(demo.ethnicity)
-  // d3.select(".panel-body").append("li").text(demo.gender)
-  // d3.select(".panel-body").append("li").text(demo.age)
-  // d3.select(".panel-body").append("li").text(demo.location)
-  // d3.select(".panel-body").append("li").text(demo.bbtype)
-  // d3.select(".panel-body").append("li").text(demo.wfreq)
-  // ;
-
-
-  
+  })
  
-  
-}}
+}
+
+function optionChanged(selectedID) {
+  makeMetaData(selectedID);
+  // MakePlots(selectedID);
+
+}
 
 
-})
 
+//---------bar chart
 
-
-//---------OR 
-
-// let demo = demoData.map((item,index) => `id ${index}`);
-// console.log(demo);
-
-//-----------OR
-
-// function demo(patient) {
-//   return patient.id === selectedID;
-// };
 // d3.json(url).then((data)=>{
-// for (let i = 0; i < 20; i++) {
-// let filteredData = demoData.filter(a => a.id == selectedID)};}
+
+//   // d3.selectAll(".col-md-5").append("div").attr("class", "panel panel-primary");
+//   d3.select("#bar").attr("class", "panel-body");
+//   d3.select("#gauge").attr("class", "panel-body");
+
+//   let sampleData = data.samples;
+//   let otuIds = [];
+//   let sampleValues = [];
+
+//   for (let i = 0; i < sampleData.length; i++) {
+
+//     let selectedSample = sampleData[i];
+//     if (selectedSample.id == selectedID ) {
+//       otuIds.push(selectedSample.otu_ids)
+//       sampleValues.push(selectedSample.sample_values)
+//       console.log(otuIds)
+//       console.log(sampleValues)};
+// }})
+
+//   let trace1 = {
+//     x: otuIds,
+//     y: sampleValues
+//   };
+  
+//   let chartData = [trace1];
+  
+//   let layout = {
+//     title: `Top 10 OTUs for ${sampleData.id}`,
+    
+//   };
+  
+// let barPlot = Plotly.newPlot("plot", chartData, layout);
+
+//  d3.select("#bar").append(barPlot);
 
 
-
-
-
-// let mapArrow3 = data.map((item, index) => `Stage ${index}: ${item}`);
-
-// console.log("Arrow3:", mapArrow3);
-
-//-----------------------code what to do when new number is selected by user
-// use samples to create for loop to update bar chart, bubble, gauge and 
-
-// use metadata to populate demo data;
+init();
